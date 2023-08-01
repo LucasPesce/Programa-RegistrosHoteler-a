453d76644eec9e30ff1e4b295102f57de6c23af9
Lucas Pesce <117691263+LucasPesce@users.noreply.github.com>
2023-08-01 11:25:24 -0300

    Add files via upload

---
+++ Registro-Reserva-Hotel/SP2/frmReserva.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace SP2
+{
+    partial class frmReserva
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textDias = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboPersonas = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboTipo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkTelevisión = new System.Windows.Forms.CheckBox();
+            this.checkHeladera = new System.Windows.Forms.CheckBox();
+            this.checkCocina = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboTarjeta = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioTarjeta = new System.Windows.Forms.RadioButton();
+            this.radioEfectivo = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textTelefono = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonAceptar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textDias);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboPersonas);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboTipo);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(483, 82);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tipo de Cabaña";
+            // 
+            // textDias
+            // 
+            this.textDias.Location = new System.Drawing.Point(378, 30);
+            this.textDias.Name = "textDias";
+            this.textDias.Size = new System.Drawing.Size(73, 20);
+            this.textDias.TabIndex = 5;
+            this.textDias.TextChanged += new System.EventHandler(this.textDias_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(340, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Días";
+            // 
+            // comboPersonas
+            // 
+            this.comboPersonas.FormattingEnabled = true;
+            this.comboPersonas.Location = new System.Drawing.Point(244, 30);
+            this.comboPersonas.Name = "comboPersonas";
+            this.comboPersonas.Size = new System.Drawing.Size(69, 21);
+            this.comboPersonas.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(179, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Personas";
+            // 
+            // comboTipo
+            // 
+            this.comboTipo.FormattingEnabled = true;
+            this.comboTipo.Location = new System.Drawing.Point(60, 31);
+            this.comboTipo.Name = "comboTipo";
+            this.comboTipo.Size = new System.Drawing.Size(100, 21);
+            this.comboTipo.TabIndex = 1;
+            this.comboTipo.SelectedIndexChanged += new System.EventHandler(this.comboTipo_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tipo";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkTelevisión);
+            this.groupBox2.Controls.Add(this.checkHeladera);
+            this.groupBox2.Controls.Add(this.checkCocina);
+            this.groupBox2.Location = new System.Drawing.Point(12, 100);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(170, 130);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Adicionales";
+            // 
+            // checkTelevisión
+            // 
+            this.checkTelevisión.AutoSize = true;
+            this.checkTelevisión.Location = new System.Drawing.Point(26, 74);
+            this.checkTelevisión.Name = "checkTelevisión";
+            this.checkTelevisión.Size = new System.Drawing.Size(74, 17);
+            this.checkTelevisión.TabIndex = 9;
+            this.checkTelevisión.Text = "Televisión";
+            this.checkTelevisión.UseVisualStyleBackColor = true;
+            // 
+            // checkHeladera
+            // 
+            this.checkHeladera.AutoSize = true;
+            this.checkHeladera.Location = new System.Drawing.Point(26, 51);
+            this.checkHeladera.Name = "checkHeladera";
+            this.checkHeladera.Size = new System.Drawing.Size(69, 17);
+            this.checkHeladera.TabIndex = 8;
+            this.checkHeladera.Text = "Heladera";
+            this.checkHeladera.UseVisualStyleBackColor = true;
+            // 
+            // checkCocina
+            // 
+            this.checkCocina.AutoSize = true;
+            this.checkCocina.Location = new System.Drawing.Point(26, 28);
+            this.checkCocina.Name = "checkCocina";
+            this.checkCocina.Size = new System.Drawing.Size(59, 17);
+            this.checkCocina.TabIndex = 7;
+            this.checkCocina.Text = "Cocina";
+            this.checkCocina.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboTarjeta);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.radioTarjeta);
+            this.groupBox3.Controls.Add(this.radioEfectivo);
+            this.groupBox3.Location = new System.Drawing.Point(194, 100);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(301, 130);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Forma de Pago";
+            // 
+            // comboTarjeta
+            // 
+            this.comboTarjeta.FormattingEnabled = true;
+            this.comboTarjeta.Location = new System.Drawing.Point(114, 74);
+            this.comboTarjeta.Name = "comboTarjeta";
+            this.comboTarjeta.Size = new System.Drawing.Size(155, 21);
+            this.comboTarjeta.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(111, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Tarjetas de Credito";
+            // 
+            // radioTarjeta
+            // 
+            this.radioTarjeta.AutoSize = true;
+            this.radioTarjeta.Location = new System.Drawing.Point(24, 50);
+            this.radioTarjeta.Name = "radioTarjeta";
+            this.radioTarjeta.Size = new System.Drawing.Size(58, 17);
+            this.radioTarjeta.TabIndex = 1;
+            this.radioTarjeta.TabStop = true;
+            this.radioTarjeta.Text = "Tarjeta";
+            this.radioTarjeta.UseVisualStyleBackColor = true;
+            this.radioTarjeta.CheckedChanged += new System.EventHandler(this.radioTarjeta_CheckedChanged);
+            // 
+            // radioEfectivo
+            // 
+            this.radioEfectivo.AutoSize = true;
+            this.radioEfectivo.Location = new System.Drawing.Point(24, 27);
+            this.radioEfectivo.Name = "radioEfectivo";
+            this.radioEfectivo.Size = new System.Drawing.Size(64, 17);
+            this.radioEfectivo.TabIndex = 0;
+            this.radioEfectivo.TabStop = true;
+            this.radioEfectivo.Text = "Efectivo";
+            this.radioEfectivo.UseVisualStyleBackColor = true;
+            this.radioEfectivo.CheckedChanged += new System.EventHandler(this.radioEfectivo_CheckedChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textTelefono);
+            this.groupBox4.Controls.Add(this.textNombre);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Location = new System.Drawing.Point(12, 236);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(483, 107);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Titular de la Reserva";
+            // 
+            // textTelefono
+            // 
+            this.textTelefono.Location = new System.Drawing.Point(98, 58);
+            this.textTelefono.Name = "textTelefono";
+            this.textTelefono.Size = new System.Drawing.Size(353, 20);
+            this.textTelefono.TabIndex = 6;
+            this.textTelefono.TextChanged += new System.EventHandler(this.textTelefono_TextChanged);
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(98, 32);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(353, 20);
+            this.textNombre.TabIndex = 5;
+            this.textNombre.TextChanged += new System.EventHandler(this.textNombre_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(23, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Telefono";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(23, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Nombre";
+            // 
+            // buttonAceptar
+            // 
+            this.buttonAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAceptar.Location = new System.Drawing.Point(390, 360);
+            this.buttonAceptar.Name = "buttonAceptar";
+            this.buttonAceptar.Size = new System.Drawing.Size(105, 32);
+            this.buttonAceptar.TabIndex = 7;
+            this.buttonAceptar.Text = "&Aceptar";
+            this.buttonAceptar.UseVisualStyleBackColor = true;
+            this.buttonAceptar.Click += new System.EventHandler(this.buttonAceptar_Click);
+            // 
+            // frmReserva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 422);
+            this.Controls.Add(this.buttonAceptar);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmReserva";
+            this.Text = "Reservas de Cabañas";
+            this.Load += new System.EventHandler(this.frmReserva_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboPersonas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboTipo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkTelevisión;
+        private System.Windows.Forms.CheckBox checkHeladera;
+        private System.Windows.Forms.CheckBox checkCocina;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboTarjeta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioTarjeta;
+        private System.Windows.Forms.RadioButton radioEfectivo;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textTelefono;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonAceptar;
+        private System.Windows.Forms.TextBox textDias;
+    }
+}
+
---
+++ Registro-Reserva-Hotel/SP2/frmReserva.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP2
+{
+    public partial class frmReserva : Form
+    {
+        //VARIABLES
+        private const float TIPO_A = 20;        //Importe por la Cabaña tipo A
+        public const float TIPO_B = 34;         //Importe por la Cabaña tipo B
+        const float COCINA = 1f;                //Importe extra por Cocina
+        const float HELADERA = 1.5f;            //Importe extra por Heladera
+        const float TELEVISOR = 2f;             //Importe extra por TV
+        const float POR_PERSONA = 1f;           //Importe extra por Persona
+
+        public frmReserva()
+        {
+            InitializeComponent();
+        }
+        //EVENTO LOAD: Se inicia una unica vez al inicio del programa.
+        //Inicializa las propiedades del formulario
+        private void frmReserva_Load(object sender, EventArgs e)
+        {
+            comboTipo.Items.Clear();            // Limpia la lista desplegable
+            comboTipo.Items.Add("Tipo A");      // Agregega a la lista el item "TIPO A"
+            comboTipo.Items.Add("Tipo B");      // Agregega a la lista el item "TIPO B"
+            comboTipo.SelectedIndex = 0;        // Establece como PREDETERMINADO el item "TIPO A"
+
+            textDias.Text = "1";                // Establece como PREDETERMINADO el 1
+
+            checkCocina.Checked = false;        // Establece como PREDETERMINADO el item DESMARCADO
+            checkHeladera.Checked = false;      // Establece como PREDETERMINADO el item DESMARCADO
+            checkTelevisión.Checked = false;    // Establece como PREDETERMINADO el item DESMARCADO
+
+            radioEfectivo.Checked = true;       // Establece como PREDETERMINADO la opcion MARCADA
+
+            comboTarjeta.Items.Clear();                     // Limpia la lista de Items
+            comboTarjeta.Items.Add("Tarjeta Naranja");      // Agrega el elemento a la lista
+            comboTarjeta.Items.Add("Tarjeta Visa");         // Agrega el elemento a la lista
+            comboTarjeta.Items.Add("Tarjeta Mastercard");   // Agrega el elemento a la lista
+
+            textNombre.Text = "";               // Limpia el campo de texto de Nombre
+            textTelefono.Text = "";             // Limpia el campo de texto de Nombre
+
+            buttonAceptar.Enabled = false;      // Deshabilita el boton aceptar
+        }
+
+        //EVENTP DE LA LISTA DE TIPO DE CABAÑA.
+        //Activacion cuando se selecciona otro tipo de cabaña
+        private void comboTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int PERSONAS = 0;      //Contador de CANTIDAD DE PERSONAS.Segun tipo de cabaña, se actualiza el contador
+            
+            comboPersonas.Items.Clear();    // Limpia el campo de Personas
+
+            if(comboTipo.SelectedIndex == 0)    // Si se escoje TIPO A: Cabañas de hasta 4 personas
+            {
+                for(PERSONAS = 1; PERSONAS <= 4; PERSONAS++) // Se ejecuta un bucle de 4 repeticiones, para agregar las opciones
+                {
+                    comboPersonas.Items.Add(PERSONAS);       // Por cada ejecucion se agrega un numero del 1-4.
+                }
+            }
+            else                               // Si no, se escogio TIPO B: Cabañas de hasta 8 personas
+            {
+                for (PERSONAS = 1; PERSONAS <= 8; PERSONAS++) // Se ejecuta un bucle de 8 repeticiones, para agregar las opciones
+                {
+                    comboPersonas.Items.Add(PERSONAS);       // Por cada ejecucion se agrega un numero del 1-8.
+                }
+            }
+            comboPersonas.SelectedIndex = 0;    // Establece como PREDETERMINADO el primer item (1 Persona)
+        }
+
+        //EVENTO DE LA SECCION DIAS.
+        private void textDias_TextChanged(object sender, EventArgs e)
+        {
+            //Condiciones para HABILITAR EL BOTON ACEPTAR. (Debe estar en las 3 secciones.)
+            if(textDias.Text != "" && textNombre.Text != "" && textTelefono.Text != "")
+            {
+                buttonAceptar.Enabled = true;
+            }
+            else
+            {
+                buttonAceptar.Enabled = false;
+            }
+        }
+
+        //EVENTO DE LA OPCION EFECTIVO.
+        private void radioEfectivo_CheckedChanged(object sender, EventArgs e)
+        {
+            comboTarjeta.Enabled = false;       // Cuando se selecciona EFECTIVO. Se deshabilita las opciones de Tarjetas.
+            comboTarjeta.SelectedIndex = -1;    // Permite ocultar las opciones.
+        }
+
+        //EVENTO DE LA OPCION TARJETA
+        private void radioTarjeta_CheckedChanged(object sender, EventArgs e)
+        {
+            comboTarjeta.Enabled = true;        // Cuando se selecciona Tarjeta. Se habilita las opciones de Tarjetas.
+            comboTarjeta.SelectedIndex = 0;     // Establece como PREDETERMINADO la primera opcion (Tarjeta Naranja)
+        }
+
+        //EVENTO DE LA SECCION NOMBRE.
+        private void textNombre_TextChanged(object sender, EventArgs e)
+        {
+            //Condiciones para HABILITAR EL BOTON ACEPTAR
+            if (textDias.Text != "" && textNombre.Text != "" && textTelefono.Text != "")
+            {
+                buttonAceptar.Enabled = true;
+            }
+            else
+            {
+                buttonAceptar.Enabled = false;
+            }
+        }
+
+        //EVENTO DE LA SECCION TELEFONO.
+        private void textTelefono_TextChanged(object sender, EventArgs e)
+        {
+            //Condiciones para HABILITAR EL BOTON ACEPTAR
+            if (textDias.Text != "" && textNombre.Text != "" && textTelefono.Text != "")
+            {
+                buttonAceptar.Enabled = true;
+            }
+            else
+            {
+                buttonAceptar.Enabled = false;
+            }
+        }
+        // EVENTO DEL BOTON ACEPTAR
+        private void buttonAceptar_Click(object sender, EventArgs e)
+        {   
+            // Variables.
+            float precioBase;
+            float opcionales;
+            float recargo;
+            int dias;
+            float total;
+
+            //DIAS
+            dias = int.Parse(textDias.Text);    // Obtiene lo ingresado en Días y lo convierte en ENTERO
+
+            if(dias < 1 || dias > 8)            // Si se selecciona por fuera de nuestras restricciones
+            {
+                MessageBox.Show("Error. Seleccione una cantidad de dias entre el 1 al 8");
+            }
+            else         // Asignacion de las variables a las opciones escogidas 
+            {
+                //TIPO DE CABAÑA
+                if(comboTipo.SelectedIndex == 0)    //Variable segun el TIPO DE CABAÑA 
+                {
+                    precioBase = TIPO_A;
+                }
+                else
+                {
+                    precioBase = TIPO_B;
+                }
+
+                // CANTIDAD DE PERSONAS
+                precioBase += (POR_PERSONA * int.Parse(comboPersonas.Text)); //Se suma el precio base a la cantidad de PERSONAS
+
+                //OPCIONALES
+                opcionales = 0; // Si no se escoge ninguna opcion, no se suma ningun valor
+                if(checkCocina.Checked == true) // Si se agrego la Cocina, se le suma el valor de esta
+                {
+                    opcionales += COCINA;
+                }
+                if (checkHeladera.Checked == true) // Si se agrego la Heladera, se le suma el valor de esta
+                {
+                    opcionales += HELADERA;
+                }
+                if (checkTelevisión.Checked == true) // Si se agrego la TV, se le suma el valor de esta
+                {
+                    opcionales += TELEVISOR;
+                }
+
+                // SUMATORIA DEL TOTAL
+                total = (precioBase + opcionales) * dias;   // Se calcula la cantidad por el total de dias
+
+                //METODO DE PAGO. SUMAR RECARGO
+                if(radioTarjeta.Checked == true)            // Si se selecciono tarjeta
+                {
+                    if(comboTarjeta.SelectedIndex == 0)     // Si se escoge Naranja
+                    {
+                        recargo = total * 0.1f;             // Se recarga el 10% al total
+                    }
+                    else                                    // Para las otras dos tarjetas
+                    {
+                        recargo = total * 0.2f;             // Se recarga el 20%
+                    }
+
+                    total += recargo;
+                }
+
+                // MUESTRA EL RESULTADO FINAL
+                MessageBox.Show("Total = US$ " + total.ToString(), "Importe de la reserva",
+                    MessageBoxButtons.OK);
+
+                // INICIALIZA LOS CONTROLES DE INTERFAZ PARA VOLVER A LOS VALORES INICIALES
+                comboTipo.SelectedIndex = 0;
+                comboPersonas.SelectedIndex = 0;
+                textDias.Text = "";
+
+                checkCocina.Checked = false;
+                checkHeladera.Checked = false;
+                checkTelevisión.Checked = false;
+
+                radioEfectivo.Enabled = true;
+
+                textNombre.Text = "";
+                textTelefono.Text = "";
+            }
+        }
+    }
+}
